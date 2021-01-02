97126e05dd243c2a35369f23e527b7fe2a17b243
miwrona2 <miwrona2@interia.pl>
2021-01-02 17:13:37 +0100

    6.3 custom renderer

---
+++ app/code/Cert/CustomizingMagentoBusinessLogic/Block/Cart/CustomRenderer.php
@@ -0,0 +1,11 @@
+<?php
+declare(strict_types=1);
+
+namespace Cert\CustomizingMagentoBusinessLogic\Block\Cart;
+
+use Magento\Checkout\Block\Cart\Item\Renderer;
+
+class CustomRenderer extends Renderer
+{
+
+}
