7d5731210ad61f98cc36f369af16b85acce06583
miwrona2 <miwrona2@interia.pl>
2021-01-17 11:09:30 +0100

    6.8 new address field; use resource model to save model; refactored

--- app/code/Cert/CustomizingMagentoBusinessLogic/Setup/Patch/Data/AddAddressField.php
+++ app/code/Cert/CustomizingMagentoBusinessLogic/Setup/Patch/Data/AddAddressField.php
@@ -4,35 +4,45 @@ declare(strict_types=1);
 namespace Cert\CustomizingMagentoBusinessLogic\Setup\Patch\Data;
 
 use Magento\Customer\Setup\CustomerSetupFactory;
+use Magento\Eav\Model\ResourceModel\Config;
 use Magento\Framework\Setup\ModuleDataSetupInterface;
 use Magento\Framework\Setup\Patch\DataPatchInterface;
-use Magento\Framework\Setup\Patch\PatchInterface;
 use Magento\Framework\Setup\Patch\PatchRevertableInterface;
 
 class AddAddressField implements DataPatchInterface, PatchRevertableInterface
 {
+    private const ATTR_CODE = 'intercom_code';
     /** @var CustomerSetupFactory */
     private $customerSetupFactory;
 
     /** @var ModuleDataSetupInterface */
     private $setup;
 
+    /** @var Config */
+    private $resourceModel;
+
     /**
      * AddAddressField constructor.
      * @param CustomerSetupFactory $customerSetupFactory
      * @param ModuleDataSetupInterface $setup
+     * @param Config $resourceModel
      */
-    public function __construct(CustomerSetupFactory $customerSetupFactory, ModuleDataSetupInterface $setup)
-    {
+    public function __construct(
+        CustomerSetupFactory $customerSetupFactory,
+        ModuleDataSetupInterface $setup,
+        Config $resourceModel
+    ) {
+
         $this->customerSetupFactory = $customerSetupFactory;
         $this->setup = $setup;
+        $this->resourceModel = $resourceModel;
     }
 
     public function apply(): void
     {
         $customerSetup = $this->customerSetupFactory->create(['setup' => $this->setup]);
-        $customerSetup->addAttribute('customer_address', 'new_field', [
-            'label' => 'New Field',
+        $customerSetup->addAttribute('customer_address', self::ATTR_CODE, [
+            'label' => 'Intercom Code',
             'input' => 'text',
             'type' => \Magento\Framework\DB\Ddl\Table::TYPE_TEXT,
             'source' => '',
@@ -49,20 +59,25 @@ class AddAddressField implements DataPatchInterface, PatchRevertableInterface
         ]);
 
         $attribute = $customerSetup->getEavConfig()
-            ->getAttribute('customer_address', 'new_field')
-            ->addData(['used_in_forms' => [
-                'adminhtml_customer_address',
-                'adminhtml_customer',
-                'customer_address_edit',
-                'customer_register_address',
-                'customer_address',
-            ]
-            ]);
-        $attribute->save();
+            ->getAttribute('customer_address', self::ATTR_CODE)
+            ->addData(
+                [
+                    'used_in_forms' => [
+                        'adminhtml_customer_address',
+                        'adminhtml_customer',
+                        'customer_address_edit',
+                        'customer_register_address',
+                        'customer_address',
+                    ]
+                ]
+            );
+        $this->resourceModel->save($attribute);
     }
 
     public function revert(): void
     {
+        $eav = $this->customerSetupFactory->create(['setup' => $this->setup]);
+        $eav->removeAttribute('customer_address', self::ATTR_CODE);
     }
 
     public function getAliases(): array
