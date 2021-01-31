3920ea5f221acdc0815b557839853367acb6d7ee
miwrona2 <miwrona2@interia.pl>
2021-01-31 10:18:59 +0100

    6.7 custom customer attribute (old way installer and uninstaller - it doesn't work)

--- app/code/Cert/CustomizingMagentoBusinessLogic/Setup/InstallData.php
+++ app/code/Cert/CustomizingMagentoBusinessLogic/Setup/InstallData.php
@@ -3,15 +3,12 @@ declare(strict_types=1);
 
 namespace Cert\CustomizingMagentoBusinessLogic\Setup;
 
-use Magento\Customer\Api\CustomerMetadataInterface;
-use Magento\Eav\Setup\EavSetup;
 use Magento\Eav\Model\Config as EavConfig;
+use Magento\Eav\Setup\EavSetup;
 use Magento\Eav\Setup\EavSetupFactory;
 use Magento\Framework\Setup\InstallDataInterface;
 use Magento\Framework\Setup\ModuleContextInterface;
 use Magento\Framework\Setup\ModuleDataSetupInterface;
-use Magento\Catalog\Model\ResourceModel\Eav\Attribute;
-use Magento\Catalog\Model\Product;
 
 class InstallData implements InstallDataInterface
 {
@@ -30,8 +27,9 @@ class InstallData implements InstallDataInterface
     public function install(ModuleDataSetupInterface $setup, ModuleContextInterface $context)
     {
         $setup->startSetup();
+        /** @var EavSetup $eavSetup */
         $eavSetup = $this->eavSetupFactory->create(['setup' => $setup]);
-        $eavSetup->removeAttribute('customer', 'interests');
+        $eavSetup->addAttribute('customer', 'interests', []);
         $setup->endSetup();
     }
 }
